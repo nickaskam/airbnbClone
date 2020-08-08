de736f8c052eda2ea6b9468fffd5e0dd25752f42
Nick Askam <asknickfilms@gmail.com>
2020-08-08 00:18:03 -0500

    adding in view component

--- packages/web/src/modules/RegisterConnector.tsx
+++ packages/web/src/modules/RegisterConnector.tsx
@@ -1,3 +1,4 @@
 import * as React from "react";
+import { RegisterView } from "../view/RegisterView";
 
-export const RegisterConnector = () => <div>register page</div>;
+export const RegisterConnector = () => <RegisterView />;
---
+++ packages/web/src/view/RegisterView.tsx
@@ -0,0 +1,7 @@
+import React, { PureComponent } from "react";
+
+export class RegisterView extends PureComponent {
+  render() {
+    return <div>hello</div>;
+  }
+}
